3d6eb17161e2ce791b78d6c33fb5465b52af1433
Anthony Watanabe <atwatanabe314@gmail.com>
2015-05-07 00:51:35 -0700

    Added code from this demo: https://android.googlesource.com/platform/development/+/master/samples/ApiDemos/src/com/example/android/apis/os/RotationVectorDemo.java Demo has functions similar to what I want in final app, so I will edit/add to the code later. For now, I set the content view for the GameActivity to only a glSurfaceView object, but I still have activity_game.xml that I will contain this glSurfaceView object later.

--- app/src/main/java/atwatanabe/realcube/GameActivity.java
+++ app/src/main/java/atwatanabe/realcube/GameActivity.java
@@ -1,17 +1,239 @@
 package atwatanabe.realcube;
 
+import android.content.Context;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.opengl.GLSurfaceView;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
 
 
-public class GameActivity extends ActionBarActivity {
+public class GameActivity extends ActionBarActivity
+{
+
+    private SensorManager mSensorManager;
+    private CubeRenderer renderer;
+    private GLSurfaceView glView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        setTitle(R.string.app_name);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        //mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        //mSensorManager.registerListener(this, mSensor, 10000);
+        renderer = new CubeRenderer();
+        glView = new GLSurfaceView(this);
+        glView.setRenderer(renderer);
+
+        //setContentView(R.layout.activity_game);
+        setContentView(glView);
+
+        /*
+        Button upLeft = (Button)findViewById(R.id.upLeft);
+        upLeft.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent m) {
+
+                Log.i("move", "upLeft");
+                return true;
+            }
+        });
+
+        Button upCenter = (Button)findViewById(R.id.upCenter);
+        upCenter.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent m)
+            {
+                Log.i("move", "upCenter");
+                return true;
+            }
+        });
+        */
+    }
+
+    @Override
+    protected void onResume() {
+        // Ideally a game should implement onResume() and onPause()
+        // to take appropriate action when the activity looses focus
+        super.onResume();
+        renderer.start();
+        glView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        // Ideally a game should implement onResume() and onPause()
+        // to take appropriate action when the activity looses focus
+        super.onPause();
+        renderer.stop();
+        glView.onPause();
+    }
+
+    /*
+    public void onSensorChanged(SensorEvent event)
+    {
+        //Log.i("onSensorChanged", event.toString());
+        if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR)
+        {
+            SensorManager.getRotationMatrixFromVector(mRotationMatrix, event.values);
+            Log.i("rotationSensor", new Float(mRotationMatrix[0]).toString());
+        }
+    }
+    */
+
+
+    class CubeRenderer implements GLSurfaceView.Renderer, SensorEventListener
+    {
+
+        private float[] mRotationMatrix = new float[16];
+        private Sensor mSensor;
+        private Cube cube;
+
+        public CubeRenderer()
+        {
+            // find the rotation-vector sensor
+            mSensor = mSensorManager.getDefaultSensor(
+                    Sensor.TYPE_ROTATION_VECTOR);
+            cube = new Cube();
+            // initialize the rotation matrix to identity
+            mRotationMatrix[ 0] = 1;
+            mRotationMatrix[ 4] = 1;
+            mRotationMatrix[ 8] = 1;
+            mRotationMatrix[12] = 1;
+        }
+
+        public void start()
+        {
+            mSensorManager.registerListener(this, mSensor, 10000);
+        }
+
+        public void stop()
+        {
+            mSensorManager.unregisterListener(this);
+        }
+
+        @Override
+        public void onSurfaceCreated(GL10 gl, EGLConfig config)
+        {
+            gl.glDisable(GL10.GL_DITHER);
+            gl.glClearColor(1, 1, 1, 1);
+        }
+
+        @Override
+        public void onSurfaceChanged(GL10 gl, int width, int height)
+        {
+            gl.glViewport(0, 0, width, height);
+            float ratio = (float) width / height;
+            gl.glMatrixMode(GL10.GL_PROJECTION);
+            gl.glLoadIdentity();
+            gl.glFrustumf(-ratio, ratio, -1, 1, 1, 10);
+        }
+
+        @Override
+        public void onDrawFrame(GL10 gl)
+        {
+            gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+            gl.glMatrixMode(GL10.GL_MODELVIEW);
+            gl.glLoadIdentity();
+            gl.glTranslatef(0, 0, -3.0f);
+            gl.glMultMatrixf(mRotationMatrix, 0);
+            gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+            gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+            cube.draw(gl);
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event)
+        {
+            //Log.i("onSensorChanged", event.toString());
+            if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR)
+            {
+                /*
+                float[] temp = new float[event.values.length];
+                for (int i = 0; i < temp.length; ++i)
+                {
+                    temp[i] = 2 * event.values[i];
+                }
+                */
+                SensorManager.getRotationMatrixFromVector(mRotationMatrix, event.values);
+                //Log.i("rotationSensor", new Float(mRotationMatrix[0]).toString());
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy)
+        {
+
+        }
+    }
+
+    class Cube
+    {
+        // initialize our cube
+        private FloatBuffer mVertexBuffer;
+        private FloatBuffer mColorBuffer;
+        private ByteBuffer mIndexBuffer;
+        public Cube() {
+            final float vertices[] = {
+                    -1, -1, -1, 1, -1, -1,
+                    1, 1, -1, -1, 1, -1,
+                    -1, -1, 1, 1, -1, 1,
+                    1, 1, 1, -1, 1, 1,
+            };
+            final float colors[] = {
+                    0, 0, 0, 1, 1, 0, 0, 1,
+                    1, 1, 0, 1, 0, 1, 0, 1,
+                    0, 0, 1, 1, 1, 0, 1, 1,
+                    1, 1, 1, 1, 0, 1, 1, 1,
+            };
+            final byte indices[] = {
+                    0, 4, 5, 0, 5, 1,
+                    1, 5, 6, 1, 6, 2,
+                    2, 6, 7, 2, 7, 3,
+                    3, 7, 4, 3, 4, 0,
+                    4, 7, 6, 4, 6, 5,
+                    3, 0, 1, 3, 1, 2
+            };
+            ByteBuffer vbb = ByteBuffer.allocateDirect(vertices.length * 4);
+            vbb.order(ByteOrder.nativeOrder());
+            mVertexBuffer = vbb.asFloatBuffer();
+            mVertexBuffer.put(vertices);
+            mVertexBuffer.position(0);
+            ByteBuffer cbb = ByteBuffer.allocateDirect(colors.length * 4);
+            cbb.order(ByteOrder.nativeOrder());
+            mColorBuffer = cbb.asFloatBuffer();
+            mColorBuffer.put(colors);
+            mColorBuffer.position(0);
+            mIndexBuffer = ByteBuffer.allocateDirect(indices.length);
+            mIndexBuffer.put(indices);
+            mIndexBuffer.position(0);
+        }
+
+        public void draw(GL10 gl) {
+            gl.glEnable(GL10.GL_CULL_FACE);
+            gl.glFrontFace(GL10.GL_CW);
+            gl.glShadeModel(GL10.GL_SMOOTH);
+            gl.glVertexPointer(3, GL10.GL_FLOAT, 0, mVertexBuffer);
+            gl.glColorPointer(4, GL10.GL_FLOAT, 0, mColorBuffer);
+            gl.glDrawElements(GL10.GL_TRIANGLES, 36, GL10.GL_UNSIGNED_BYTE, mIndexBuffer);
+        }
     }
 
 
@@ -21,6 +243,7 @@ public class GameActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_game, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
--- app/src/main/java/atwatanabe/realcube/OptionsActivity.java
+++ app/src/main/java/atwatanabe/realcube/OptionsActivity.java
@@ -2,20 +2,69 @@ package atwatanabe.realcube;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.content.SharedPreferences;
 /**
  * Created by Anthony on 4/16/2015.
  */
 public class OptionsActivity extends ActionBarActivity
 {
+    private static final String PREFERENCES = "PreferencesFile";
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+
+        //cubeManipProgress = cubeRotateProgress = 50;    //load values
+
+        SharedPreferences settings = getPreferences(MODE_PRIVATE);
+        int cubeManipProgress = settings.getInt("cubeManip", 50),
+            cubeRotateProgress = settings.getInt("cubeRotate", 50);
+
+
+        final SeekBar manipBar = (SeekBar) findViewById(R.id.cubeManip);
+        manipBar.setProgress(cubeManipProgress);
+
+        final SeekBar rotateBar = (SeekBar) findViewById(R.id.cubeRotate);
+        rotateBar.setProgress(cubeRotateProgress);
+
+        Button defaultsButton = (Button)findViewById(R.id.restoreDefaultsButton);
+        defaultsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                manipBar.setProgress(50);
+                rotateBar.setProgress(50);
+            }
+        });
+
+    }
+
+
+
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        SharedPreferences settings = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+
+
+        SeekBar manipBar = (SeekBar) findViewById(R.id.cubeManip);
+        SeekBar rotateBar = (SeekBar) findViewById(R.id.cubeRotate);
+
+        editor.putInt("cubeManip", manipBar.getProgress());
+        editor.putInt("cubeRotate", rotateBar.getProgress());
+        editor.commit();
     }
 
     @Override
